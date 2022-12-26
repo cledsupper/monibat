65b5ea9582ac6c3794970f061314265945999f54
Cledson Ferreira <cledsonitgames@gmail.com>
2022-12-26 10:10:50 -0300

    Boot script and README issues

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
+if [ "$(id -u)" = "0" ];
+  echo "NÃO EXECUTE COMO ROOT!"
+  exit 1
+fi
+
 TARGET="$PREFIX/lib/MoniBat"
 mkdir -p "$TARGET/config"
 mkdir -p "$TARGET/data"
@@ -18,3 +23,6 @@ if [ ! -r "$HOME/.config/MoniBat/config.json" ]; then
   cp samples/config.json "$HOME/.config/MoniBat/"
   echo "Defina a capacidade (em Ampère-hora/Ah) no arquivo: $HOME/.config/MoniBat/config.json"
  fi
+
+mkdir -p "$HOME/.termux/boot"
+cp moni.sh "$HOME/.termux/boot/"
