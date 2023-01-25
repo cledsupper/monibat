0a61975984cf86459091e7fd1897c05c89030c61
Cledson Ferreira <cledsonitgames@gmail.com>
2023-01-25 01:28:18 -0300

    Assert integer percent

--- batterydriver.py
+++ batterydriver.py
@@ -101,6 +101,7 @@ class Battery(BatteryEmulator):
         self._sp_data = json.loads(text)
         if self._td_up:
             self._percent = 1 + 100*(self._td_eng / self._td_cap)
+            self._percent = int(self._percent)
             self._capacity = self._td_cap / 1000
             self._energy_now = self._td_eng / 1000
         else:
